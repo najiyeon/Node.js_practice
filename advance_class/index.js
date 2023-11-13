5623cbb75298acfd916f3a6fb28269e008b97f82
Jiyeon Na <skwldus@hanyang.ac.kr>
2023-11-13 18:40:32 +0900

    add: node.js skeleton

---
+++ advance_class/index.js
@@ -0,0 +1,24 @@
+'use strict'
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+const serverPort = 80;
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extends: false}));
+app.use(cors());
+
+const router = express.Router();
+
+const ping = (req, res) => {
+    const result = "pong";
+    res.send({result});
+    console.log("ping is called");
+}
+
+router.route('/ping').get(ping);
+
+app.use('/test/api/v1', router);
+app.listen(serverPort);
